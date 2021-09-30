ecc2ab94100daf08d354b6958379bc84114df8ea
Isco <deftoner.javier@gmail.com>
2021-09-30 00:20:28 -0500

    Se agregaron las funciones a la tabla

--- example/example.js
+++ example/example.js
@@ -18,27 +18,22 @@ import {
 } from './../dist/index.js'*/
 
 export default () => {
-    console.log( "TEXT >>>",        textValidate( "forever" ) )
-    console.log( "TEXT_NUM >>>",    textNumberValidate( "ñáäéëíïóöúüÑÁÄÉËÍÏÓÖÚÜ" ) )
-    console.log( "RFC >>>",         rfcValidate( "XXXX920804XX1" ) )
-    console.log( "RFC_CLAVE >>>",   rfcValidateClave( "XXXX920807" ) )
-    console.log( "EMAIL >>>",       emailValidate( "fer.l.c@gmail.com" ) )
-    console.log( "REQUIRED >>>",    requiredData( "aabc" ) )
-    console.log( "COMBO >>>",       comboValidate( "1" ) )
-    console.log( "RADIO >>>",       radioButtonValidate( true ) );
-    console.log( "DATE_TO_RFC >>>", validationDateRFC( 'XXXX920804LFP', '04-08-1992', 'R.F.C.', 'Fecha de nacimiento', 'errorRFC', 'errorFecha', 'focusRFC', 'focusDate' ) );
-    console.log( "DATE >>>",        dateValidateFormat( '31/02/1991' ) );
-    console.log( "DATE >>>",        dateValidateFormat( '31-02-1991' ) );
-    console.log( "DATE >>>",        dateValidateFormat( '31.02.1991' ) );
-    console.log( "COMMON >>>",      commonValidate( "hyukjhg", /^([a-zA-Z ñáéíóúÑÁÉÍÓÚ .,]{0,100})$/ ) );
-    console.log( "SPECIAL_CHA >>>", textAreSpecialValidate( ":;#/()&\-_*,." ) );
-    console.log( "SPECIAL_IN >>>",  specialCharacterInValidate( ":;#/()&\-_*,." ) );
 
-
-    const [ numValue, setNumValue ]   = useState(12123);
-    const [ checValue, setChecValue ] = useState( { 1 : true } );
-    const [ textValue, setTextValue ] = useState( 'Hola Mundo' );
-
+    const [ numValue, setNumValue ]                                          = useState( 0 );
+    const [ checValue, setChecValue ]                                        = useState( { 1 : true } );
+    const [ textValue, setTextValue ]                                        = useState( 'Hola Mundo' );
+    const [ textValueNumberValidate, setTextNumberValidate ]                 = useState( "ñáäéëíïóöúüÑÁÄÉËÍÏÓÖÚÜ" )
+    const [ rfcValueValidate, setRfcValueValidate ]                          = useState( "XXXX920804XX1" )
+    const [ rfcValueValidateClave, setRfcValidateClave ]                     = useState( "XXXX920807" )
+    const [ emailValueValidate, setEmailValidate ]                           = useState( "fer.l.c@gmail.com" )
+    const [ requiredDataValue, setRequiredData ]                             = useState( "aabc" )
+    const [ comboValidateValue, setComboValidate ]                           = useState("1")
+    const [ radioButtonValidateValue, setRadioButtonValidate ]               = useState( true )
+    const [ validationDateRFCValue, setValidationDateRFC ]                   = useState( 'XXXX920804LFP', '04-08-1992', 'R.F.C.', 'Fecha de nacimiento', 'errorRFC', 'errorFecha', 'focusRFC', 'focusDate' )
+    const [ dateValidateFormatValue, setSateValidateFormat ]                 = useState( '31/02/1991' )
+    const [ commonValidateValue, setCommonValidate ]                         = useState( "hyukjhg", /^([a-zA-Z ñáéíóúÑÁÉÍÓÚ .,]{0,100})$/ )
+    const [ textAreSpecialValidateValue, setTextAreSpecialValidate ]         = useState( ":;#/()&\-_*,." )
+    const [ specialCharacterInValidateValue, setSpecialCharacterInValidate ] = useState( ":;#/()&\-_*,." )
 
     const [ multiValue, setMultiValue ]   = useState( DATA );
     const [ singleValue, setSingleValue ] = useState( DATA[3] );
@@ -64,6 +59,90 @@ export default () => {
             value         : textValue,
             funcion       : textValidate,
             hook          : setTextValue,
+            aplyParse     : false
+        },
+        {
+            nombreFuncion : `textNumberValidate( "ñáäéëíïóöúüÑÁÄÉËÍÏÓÖÚÜ" )`,
+            value         : textValueNumberValidate,
+            funcion       : textNumberValidate,
+            hook          : setTextNumberValidate,
+            aplyParse     : false
+        },
+        {
+            nombreFuncion : `rfcValidate( "XXXX920804XX1" )`,
+            value         : rfcValueValidate,
+            funcion       : rfcValidate,
+            hook          : setRfcValueValidate,
+            aplyParse     : false
+        },
+        {
+            nombreFuncion : `rfcValidateClave( "XXXX920804" )`,
+            value         : rfcValueValidateClave,
+            funcion       : rfcValidateClave,
+            hook          : setRfcValidateClave,
+            aplyParse     : false
+        },
+        {
+            nombreFuncion : `emailValidate( "fer.l.c@gmail.com" )`,
+            value         : emailValueValidate,
+            funcion       : emailValidate,
+            hook          : setEmailValidate,
+            aplyParse     : false
+        },
+        {
+            nombreFuncion : `requiredData( "aabc" )`,
+            value         : requiredDataValue,
+            funcion       : requiredData,
+            hook          : setRequiredData,
+            aplyParse     : false
+        },
+        {
+            nombreFuncion : `comboValidate( "1" )`,
+            value         : comboValidateValue,
+            funcion       : comboValidate,
+            hook          : setComboValidate,
+            aplyParse     : false
+        },
+        { /* FIXME: Revisar como regresar boolean */
+            nombreFuncion : `radioButtonValidate( true )`,
+            value         : radioButtonValidateValue,
+            funcion       : radioButtonValidate,
+            hook          : setRadioButtonValidate,
+            aplyParse     : false
+        },
+        { /* FIXME: Revisar como mandarle diferentes hooks */
+            nombreFuncion : `validationDateRFC( 'XXXX920804LFP', '04-08-1992', 'R.F.C.', 'Fecha de nacimiento', 'errorRFC', 'errorFecha', 'focusRFC', 'focusDate' )`,
+            value         : validationDateRFCValue,
+            funcion       : validationDateRFC,
+            hook          : setValidationDateRFC,
+            aplyParse     : false
+        },
+        {
+            nombreFuncion : `dateValidateFormat( '31/02/1991' ), dateValidateFormat( '31-02-1991' ) ), dateValidateFormat( '31.02.1991' ) ) `,
+            value         : dateValidateFormatValue,
+            funcion       : dateValidateFormat,
+            hook          : setSateValidateFormat,
+            aplyParse     : false
+        },
+        {
+            nombreFuncion : `commonValidate( "hyukjhg", /^([a-zA-Z ñáéíóúÑÁÉÍÓÚ .,]{0,100})$/ )  `,
+            value         : commonValidateValue,
+            funcion       : commonValidate,
+            hook          : setCommonValidate,
+            aplyParse     : false
+        },
+        {
+            nombreFuncion : `textAreSpecialValidate( ":;#/()&\-_*,." )  `,
+            value         : textAreSpecialValidateValue,
+            funcion       : textAreSpecialValidate,
+            hook          : setTextAreSpecialValidate,
+            aplyParse     : false
+        },
+        {
+            nombreFuncion : `specialCharacterInValidate( ":;#/()&\-_*,." )  `,
+            value         : specialCharacterInValidateValue,
+            funcion       : specialCharacterInValidate,
+            hook          : setSpecialCharacterInValidate,
             aplyParse     : false
         },
         {
